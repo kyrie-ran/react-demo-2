b72d761dae1e510f880aa012a95ec0c4e39e38f3
陈浩然 <1638238157@qq.com>
2021-09-22 16:08:51 +0800

    antd form

--- lesson1/src/App.js
+++ lesson1/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-
+import FromAntd from './components/FromAntd';
 function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+    return (
+        <div className="App">
+            <FromAntd />
+        </div>
+    );
 }
 
 export default App;
---
+++ lesson1/src/components/FromAntd.js
@@ -0,0 +1,78 @@
+// import React, { Component } from 'react';
+import React from 'react';
+import { Form, Input, Button } from 'antd';
+
+const FromAntd = () => {
+    const onFinish = (values) => {
+        console.log('Success:', values);
+    };
+
+    const onFinishFailed = (errorInfo) => {
+        console.log('Failed:', errorInfo);
+    };
+
+    return (
+        <Form
+            name="basic"
+            labelCol={{
+                span: 8,
+            }}
+            wrapperCol={{
+                span: 16,
+            }}
+            initialValues={{
+                remember: true,
+            }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+        >
+            <Form.Item
+                label="Username"
+                name="username"
+                rules={[
+                    {
+                        required: true,
+                        message: 'Please input your username!',
+                    },
+                ]}
+            >
+                <Input />
+            </Form.Item>
+
+            <Form.Item
+                label="Password"
+                name="password"
+                rules={[
+                    {
+                        required: true,
+                        message: 'Please input your password!',
+                    },
+                ]}
+            >
+                <Input.Password />
+            </Form.Item>
+
+            <Form.Item
+                wrapperCol={{
+                    offset: 8,
+                    span: 16,
+                }}
+            >
+                <Button type="primary" htmlType="submit">
+                    Submit
+                </Button>
+            </Form.Item>
+        </Form>
+    );
+};
+export default FromAntd;
+// export default class FromAntd extends Component {
+//     render() {
+//         return (
+//             <div>
+
+//             </div>
+//         )
+//     }
+// }
