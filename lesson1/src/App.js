25746f3667ea0fff26ab14d78bb6de6bc3d3301e
陈浩然 <1638238157@qq.com>
2021-09-23 13:57:22 +0800

    Dialog

--- lesson1/src/App.js
+++ lesson1/src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 // import FromAntd from './components/FromAntd';
 // import MyRCFieldForm from './pages/MyRcFieldForm';
-import MyRCForm from './pages/MyRCForm';
+// import MyRCForm from './pages/MyRCForm';
+import DialogPage from './pages/DialogPage';
 function App() {
     return (
         <div className="App">
             {/* <FromAntd /> */}
             {/* <MyRCFieldForm /> */}
-            <MyRCForm />
+            {/* <MyRCForm /> */}
+            <DialogPage />
         </div>
     );
 }
