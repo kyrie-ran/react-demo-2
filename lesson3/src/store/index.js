049e45287ea9e24e3459f0695c029443f246ab8f
陈浩然 <1638238157@qq.com>
2021-09-26 13:20:25 +0800

    init lesson3 and add react-redux

--- lesson3/src/store/index.js
+++ lesson3/src/store/index.js
@@ -1,10 +1,7 @@
-// import { createStore, applyMiddleware ,combineReducers} from 'redux';
-import { createStore, applyMiddleware, combineReducers } from '../kredux';
-// import thunk from "redux-thunk"; // 异步解决方案
-// import logger from "redux-logger"; // 打印日志
-// import promise from "redux-promise"; // 处理promise
-import isPromise from "is-promise"; // 检查是不是promise
-import { isFSA } from "flux-standard-action"; // 检查是不是标准的action模式
+import { createStore, applyMiddleware ,combineReducers} from 'redux';
+import thunk from "redux-thunk"; // 异步解决方案
+import logger from "redux-logger"; // 打印日志
+import promise from "redux-promise"; // 处理promise
 
 export const counterReducer = (state = 0, { type, payload = 1 }) => {
     switch (type) {
@@ -37,56 +34,3 @@ const store = createStore(
 );
 
 export default store;
-
-function logger({ dispatch, getState }) {
-    return next => action => {
-        console.log('++++++++++++++');
-        console.log(action.type + '执行了~');
-        const prevState = getState();
-        console.log("prev state", prevState);
-
-        const returnValue = next(action);
-
-        const nextState = getState();
-        console.log("next state", nextState);
-
-
-        console.log('++++++++++++++');
-
-        return returnValue;
-    }
-}
-
-function thunk({ dispatch, getState }) {
-    return next => action => {
-        // action 类型  对象 | 函数
-        if (typeof action === 'function') {
-            return action(dispatch, getState);
-        }
-        return next(action)
-    }
-}
-
-// 简版
-// function promise({ dispatch }) {
-//     return next => action => {
-//         return isPromise(action) ? action.then(dispatch) : next(action);
-//     }
-// }
-
-// 完整版
-function promise({ dispatch }) {
-    return next => action => {
-        if (!isFSA(action)) {
-            return isPromise(action) ? action.then(dispatch) : next(action);
-        }
-        return isPromise(action.payload) ?
-            action.payload
-                .then(result => dispatch({ ...action, payload: result }))
-                .catch(error => {
-                    dispatch({ ...action, payload: error, error: true });
-                    return Promise.reject(error);
-                })
-            : next(action);
-    }
-}
